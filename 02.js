4e78e8e31f3e1033eac772f65e2019cd8ed4c276
Manuel Carrera <manuel.carrera@workday.com>
2017-11-18 18:38:31 -0700

    solving more problems

---
+++ 02.js
@@ -0,0 +1,7 @@
+const array = [1,2,'orange', 5, 6]
+
+function secondToLastEl(arr) {
+  return console.warn(arr[arr.length -2])
+}
+
+secondEl(array)
---
+++ 03.js
@@ -0,0 +1,7 @@
+const array = [1,2,3,4,5]
+
+function findNthElement(arr, index) {
+  return console.warn(arr[index -1])
+}
+
+findNthElement(array, 3)
