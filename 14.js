3dcfc535bd2e9d4f5f6ea319ae653619c15e2c4a
Manuel Carrera <manuel.carrera@workday.com>
2017-12-18 13:07:04 -0700

    add decrypt

--- 14.js
+++ 14.js
@@ -1,3 +1,9 @@
+/**
+ * Encrypts a string based on capital letters.
+ * @param  {[type]} str   the string you want to encrypt
+ * @param  {[type]} shift by how many letters you want to shift your encryption
+ */
+
 function encrypt(str, shift) {
   const upperCaseStr = str.toUpperCase() // only account for uppercase letters
   let encryptedString = []
@@ -15,4 +21,24 @@ function encrypt(str, shift) {
   }
   return encryptedString.join('')
 }
-console.log(encrypt("yourmomiscool", 5))
+
+function deCrypt(str, shift) {
+  const upperCaseStr = str.toUpperCase() // only account for uppercase letters
+  let decryptedString = []
+
+  for (var i = 0; i < upperCaseStr.length; i++) {
+    let shiftLetters = upperCaseStr.charCodeAt(i) - shift
+    let newCharValue = String.fromCharCode(shiftLetters)
+
+    if(shiftLetters < 65) {
+      const newIndex = (shiftLetters - 65) + 1
+      newCharValue  = String.fromCharCode(newIndex + 90)
+    }
+
+    decryptedString.push(newCharValue)
+  }
+  return decryptedString.join('')
+}
+
+console.log(deCrypt('DTZWRTRNXHTTQD', 5))
+console.log(encrypt("yourmomiscooly", 5))
